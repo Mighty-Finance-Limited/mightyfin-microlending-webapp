c1da80a9127e45b33f7564299af2ea4503985c7a
Andrew Sithole <andrewsithole7@gmail.com>
2024-08-05 15:11:37 +0200

    Initial commit

---
+++ app/Http/Controllers/Auth/OTPController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OTPController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(auth()->user()->opt_verified == 0){
+            return view('auth.otp_verification');
+        }else{
+            return redirect()->route('dashboard');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function verifyOTP(Request $request)
+    {
+        $user = User::where('id', $request->input('user_id'))->first();
+        try {
+            if ($user->opt_code == $request->input('otp')) {
+                $user->update([
+                    'opt_verified' => 1
+                ]);
+                return response()->json('true');
+            } else {
+                return response()->json('false');
+            }
+        } catch (\Throwable $th) {
+            return response()->json('false');
+        }
+    }
+}
